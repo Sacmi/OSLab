15466c21169e40a096b7408de50f036701f4d51f
Igor Anufriev <sacmi@pm.me>
2021-04-12 17:49:12 +0700

    2 лаба

---
+++ 5.c
@@ -0,0 +1,92 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd[2][2], result;
+    size_t size;
+    char resstring[14];
+
+    if (pipe(fd[0]) < 0 || pipe(fd[1]) < 0)
+    {
+        printf("Can\'t create pipe\n");
+        exit(-1);
+    }
+
+    result = fork();
+
+    if (result < 0)
+    {
+
+        printf("Can\'t fork child\n");
+        exit(-1);
+    }
+    else if (result > 0)
+    {
+
+        close(fd[0][0]);
+
+        size = write(fd[0][1], "Hello, world!", 14);
+
+        if (size != 14)
+        {
+            printf("Can\'t write all string\n");
+            exit(-1);
+        }
+
+        close(fd[0][1]);
+
+        // чтение
+
+        close(fd[1][1]);
+
+        size = read(fd[1][0], resstring, 14);
+
+        if (size < 0)
+        {
+
+            printf("Can\'t read string\n");
+            exit(-1);
+        }
+
+        printf("%s\n", resstring);
+
+        close(fd[1][0]);
+        printf("Parent exit\n");
+    }
+    else
+    {
+
+        close(fd[0][1]);
+
+        size = read(fd[0][0], resstring, 14);
+
+        if (size < 0)
+        {
+
+            printf("Can\'t read string\n");
+            exit(-1);
+        }
+
+        printf("%s\n", resstring);
+
+        close(fd[0][0]);
+
+        // запись
+
+        close(fd[1][0]);
+
+        size = write(fd[1][1], "Hello, world!", 14);
+
+        if (size != 14)
+        {
+            printf("Can\'t write all string\n");
+            exit(-1);
+        }
+
+        close(fd[1][1]);
+    }
+
+    return 0;
+}
---
+++ 7-2.c
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int fd;
+    char resstring[14];
+    char name[] = "aaa.fifo";
+
+    (void)umask(0);
+
+    if ((fd = open(name, O_RDONLY)) < 0)
+    {
+        printf("Can\'t open FIFO for reading\n");
+        exit(-1);
+    }
+
+    if (read(fd, resstring, 14) != 14)
+    {
+
+        printf("Can\'t read string\n");
+        exit(-1);
+    }
+
+    printf("%s\n", resstring);
+    close(fd);
+
+    return 0;
+}
